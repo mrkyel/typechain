13747ab206c02fd361823d13d21eef6c89b81c49
mrkyel <44009984+mrkyel@users.noreply.github.com>
2019-10-02 23:44:30 +0900

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-alert("hello");
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+};
+
+console.log(sayHi("Nicolas", 24, "male"));
+
+export {};
