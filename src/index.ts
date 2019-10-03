62aa0c3980e36ba1e53fad480a9f6eb204a1d33d
mrkyel <44009984+mrkyel@users.noreply.github.com>
2019-10-04 00:00:44 +0900

    191004

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
-};
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
 
-console.log(sayHi("Nicolas", 24, "male"));
+const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
+
+let blockchain: [Block] = [genesisBlock];
+
+console.log(blockchain);
 
 export {};
